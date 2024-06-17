658786709f9b5cf7785826d8be4a33bc6370349b
CatraMyBeloved <ole.stein@alumni.fh-aachen.de>
2024-06-17 13:24:00 +0200

    Create display_test.ino

---
+++ display_test/display_test.ino
@@ -0,0 +1,30 @@
+#include "Adafruit_GFX.h"
+#include <Adafruit_SSD1306.h>
+#include <Wire.h>
+
+Adafruit_SSD1306 display(128,32,&Wire,-1);
+
+void setup(){
+  Serial.begin(9600);
+  delay(1000);
+  if(display.begin()){
+    Serial.println("Display has been connected and initialized.");
+  }
+  //Shows content of buffer on screen
+  display.display();
+  delay(5000);
+}
+void loop(){
+  display.clearDisplay();
+  display.display();
+  delay(2000);
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,8);
+  display.println(F("test 1 2 3 4 "));
+  display.println(F("need water :3"));
+  display.display();
+  display.startscrollright(0x00, 0x0F);
+  delay(10000);
+  display.stopscroll();
+}
